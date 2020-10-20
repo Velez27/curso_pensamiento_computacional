42ac734467e65dcfa09a405cab687cb2e3157ee1
Jesus Velez <jvf_velez@hotmail.com>
2020-10-20 17:08:35 -0500

    Se empieza el programa para convertir de texto a binario

---
+++ converter_text_to_binary.py
@@ -0,0 +1,11 @@
+def run():
+    text = input('Escribe el texto que deseas convertir a binario: ')
+
+    potency = [128, 64, 32, 8, 4, 2, 1]
+
+    asciiCodeArray = []
+    
+
+
+if __name__ == '__main__':
+    run()
